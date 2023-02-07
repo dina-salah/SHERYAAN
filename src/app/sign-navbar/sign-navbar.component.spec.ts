37704fe06b78dbd1006f302e39b268fc3b0c3889
dina-salah <dinasalah35586@gmail.com>
2023-02-07 17:06:56 +0200

    delet filter and old navbar comp update both navbars

--- src/app/sign-navbar/sign-navbar.component.spec.ts
+++ src/app/sign-navbar/sign-navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { SignNavbarComponent } from './sign-navbar.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('SignNavbarComponent', () => {
+  let component: SignNavbarComponent;
+  let fixture: ComponentFixture<SignNavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ SignNavbarComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(SignNavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/sign-navbar/sign-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-navbar',
+  templateUrl: './sign-navbar.component.html',
+  styleUrls: ['./sign-navbar.component.css']
+})
+export class SignNavbarComponent {
+
+}
