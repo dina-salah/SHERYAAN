597895b5f9df2478ac3f4e854cdfbde279d1e4ae
dina-salah <dinasalah35586@gmail.com>
2023-02-17 01:00:17 +0200

    added some features in the user profile comp

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+  displays:boolean=false;
+  displayd:boolean=true;
+
+  user!:  {fname: string;
+    lname: string;
+    ssn: string;
+    gender: string;
+    age: string;
+    address: string;
+    phone: string;
+    email: string;
+    city: string;
+    bloodType: string;
+    healthstatus: string;
+    password: string;}
+
+  
+    show(){
+      if(this.displays==false ||this.displayd==true){
+        this.displays=true
+        this.displayd=false
+      }else{
+        this.displays=false
+        this.displayd=true
+      }
+    }
 
 }
