ad56ee34db586e0b4eb538924da1abbce197f55d
iamalaa <ashouralaa23@gmail.com>
2023-03-14 18:32:11 +0200

    modified home and added some files

---
+++ src/app/blood-drives-home/blood-drives-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blood-drives-home',
+  templateUrl: './blood-drives-home.component.html',
+  styleUrls: ['./blood-drives-home.component.css']
+})
+export class BloodDrivesHomeComponent {
+
+}
