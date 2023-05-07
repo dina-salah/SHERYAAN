a7458e965a22beb0d2eaf838efe99dd938655faf
dina-salah <dinasalah35586@gmail.com>
2023-05-07 16:57:39 +0200

    ADD some search functionallities and search service

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { search } from '../services/search.service';
@@ -12,29 +12,40 @@ import { switchMap, catchError, tap } from 'rxjs/operators';
   styleUrls: ['./search.component.css']
 })
 
-export class SearchComponent {
+export class SearchComponent implements OnInit{
 @Input() display:any;
+
+constructor(private searchservice:search ){
+  this.searchservice.getStock('search')
+  .subscribe((v)=>{
+    console.log(v)
+  }
+  )
+}
+ngOnInit(): void {
+}
 
 searchform : FormGroup = new FormGroup ({
   search : new FormControl('')
 })
 
+
+
 public hospitalList : Array<any> =[]
 
-constructor(private searchservice:search ){
-  this.searchform.get('search').valueChanges
-  .pipe(
-    debounceTime(1000),
-    distinctUntilChanged(),
-    switchMap((v)=>this.searchservice.getStock(v)),
-  )
-  .subscribe(
-    (v)=>{
-      // hospital stock that value must be passed from html file
-      this.hospitalList= v?.hospitalstock;
+// constructor(private searchservice:search ){
+//   this.searchform.get('search').valueChanges
+//   .pipe(
+//     debounceTime(1000),
+//     distinctUntilChanged(),
+//     switchMap((v)=>this.searchservice.getStock(v)),
+//   )
+//   .subscribe(
+//     (v)=>{
+//       // hospital stock that value must be passed from html file
+//       this.hospitalList= v?.hospitalstock;
+//     }
+//   )
+// }
 
-    }
-  )
-
-}
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -9,14 +9,14 @@ import { Observable, map, catchError, throwError } from 'rxjs';
 
 export class search{
 
-private url:string='';
+private url:string='http://localhost:1000/stock-search';
 
 constructor(private http:HttpClient){
 
 }
-//checkout the api and the way link written
-getStock(hospitalName:string) : Observable<any>{
-    return this.http.get(`${this.url}/${hospitalName}`)
+
+getStock(name:any): Observable<any>{
+    return this.http.post<any>(this.url,name);
 }
 
 }
