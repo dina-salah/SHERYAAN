59ba43d669f2fd7f43b99732290f044e6bb27467
dina-salah <dinasalah35586@gmail.com>
2023-06-06 22:16:55 +0200

    req/req-from copm added

--- src/app/donate-now/donate-now.component.ts
+++ src/app/donate-now/donate-now.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { City,reqAdd } from '../model/request';
+import { addRequestService } from '../services/addRequest.service';
 
 @Component({
   selector: 'app-donate-now',
   templateUrl: './donate-now.component.html',
   styleUrls: ['./donate-now.component.css']
 })
-export class DonateNowComponent {
+export class DonateNowComponent implements OnInit{
+  patients?: reqAdd[];
+
+
+
+constructor(private service: addRequestService){}
+
+ngOnInit(){
+
+  this.service.retriveAllReq()
+  .subscribe({
+   next: (res)=>{
+    this.patients = res.data;
+    console.log(this.patients);
+    },error:(error)=>{
+      console.log(error)
+    }
+  })
+}
+  // onDonate(p: any){
+  //   this.donatedata.request_id = p.request_id;
+  //   this.donatedata.user_id = JSON.parse(this.id);
+  //   console.log(this.donatedata)
+  //   this.service.donate(this.donatedata).subscribe((res) => {
+  //     console.log(res);
+  //   })
+  // }
 
 }
